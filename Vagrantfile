3bf1824ba9b870dfb3545f879e9f5be8daa8feb1
Alexander Böhn <alexander@objectsinspaceandtime.com>
2012-01-07 10:54:26 -0500

    Added Puppet config for lucid64 (putative!) and edited the Vagrantfile."

--- Vagrantfile
+++ Vagrantfile
@@ -1,27 +1,31 @@
+
+
 Vagrant::Config.run do |config|
   # All Vagrant configuration is done here. The most common configuration
   # options are documented and commented below. For a complete reference,
   # please see the online documentation at vagrantup.com.
 
   # Every Vagrant virtual environment requires a box to build off of.
-  config.vm.box = "lucid32"
+  config.vm.box = "lucid64"
   config.vm.provision :puppet, :module_path => "modules"
+  
   # The url from where the 'config.vm.box' box will be fetched if it
   # doesn't already exist on the user's system.
-  # config.vm.box_url = "http://domain.com/path/to/above.box"
+  config.vm.box_url = "http://files.vagrantup.com/lucid64.box"
 
   # Boot with a GUI so you can see the screen. (Default is headless)
   # config.vm.boot_mode = :gui
 
   # Assign this VM to a host only network IP, allowing you to access it
   # via the IP.
-  # config.vm.network "33.33.33.10"
+  config.vm.network "33.33.33.10"
 
   # Forward a port from the guest to the host, which allows for outside
   # computers to access the VM, whereas host only networking does not.
-  # config.vm.forward_port "http", 80, 8080
+  config.vm.forward_port "http", 80, 8080
   config.vm.forward_port "django", 8000, 8000
-  config.vm.forward_port "devserver", 8080, 8080
+  
+  #config.vm.forward_port "devserver", 8080, 8080
 
   # Share an additional folder to the guest VM. The first argument is
   # an identifier, the second is the path on the guest to mount the
---
+++ manifests/lucid64.pp
@@ -0,0 +1,83 @@
+Exec{path=>"/usr/bin:/usr/sbin"}
+stage { "update": before => Stage["pre"] }
+stage { "pre": before => Stage["main"] }
+
+class aptupdate {
+      exec { "/usr/bin/apt-get update":}
+}
+
+class python {
+    package {
+        "build-essential": ensure => latest;
+        "python": ensure => "2.6.5-0ubuntu1";
+        "python-dev": ensure => "2.6.5-0ubuntu1";
+        "python-setuptools": ensure => installed;
+	"libreadline5-dev": ensure => present;
+	"libssl-dev": ensure => present;
+	"python-openssl": ensure => present;
+	"sqlite3": ensure => present;
+	"libsqlite3-dev": ensure => present;
+    }
+    exec { "easy_install pip":
+        path => "/usr/local/bin:/usr/bin:/bin",
+        refreshonly => true,
+        require => Package["python-setuptools"],
+        subscribe => Package["python-setuptools"],
+    }
+}
+
+class pildeps {
+  package { 
+  "libbz2-dev": ensure => present;
+  "zlib1g-dev": ensure => present;
+  "libfreetype6-dev": ensure => present;
+  "libjpeg62-dev": ensure => present;
+  "liblcms1-dev": ensure => present;
+  }
+}
+
+class vcs {
+  package { "git-core":
+    ensure => present,
+  }
+}
+class checkoutbuildout {
+  subversion::working-copy {
+    "buildout":
+      path => "/opt/python",
+      branch => "bda-naked-python/",
+      owner => "root",
+      group => "root",
+      repo_base => "svn.plone.org/svn/collective",
+      require => Package["python-setuptools"];
+  }
+}
+
+class buildpythons {
+  include checkoutbuildout
+  exec { "/usr/bin/python2.6 /opt/python/bootstrap.py --distribute  && /opt/python/bin/buildout -c all.cfg":
+      user => "root",
+      cwd => "/opt/python",
+      path => "/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin",
+      timeout => 7200,
+      require => [Class["checkoutbuildout"],Class["python"],Class["pildeps"]],
+      creates => "/opt/python/bin/buildout",
+      logoutput => on_failure,
+      
+  }
+
+}
+class { "aptupdate": stage => "update"}
+class { "python": stage => "pre" }
+class { "pildeps": stage => "pre" }
+class { "vcs": stage => "pre" }
+
+class lucid64 {
+  include aptupdate
+  include python
+  include pildeps
+  include vcs
+  include buildpythons
+}
+
+include lucid64
